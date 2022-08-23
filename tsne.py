965c9fdaa0127568fd542b80c63b4fa450bfd924
shijiawei <1371664923@qq.com>
2022-08-23 16:47:12 +0800

    add README

--- tsne.py
+++ tsne.py
@@ -71,66 +71,10 @@ std = torch.from_numpy(np.concatenate(std, axis=0))
 
 
 if __name__ == "__main__":
-    # dir = 'img'
-    # data_transforms = transforms.Compose([
-    #     transforms.ToPILImage(),
-    #     transforms.Resize((1080, 1920)),
-    #     transforms.ToTensor(),
-    # ])
-
-    # dataset = ImagePathDataset(dir, label_dict, data_transforms)
-    # print(len(dataset))
-    # # dataset.check_paths()
-    # print(f"Total num: {len(dataset)}.")
-    # dataloader = data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=0)
-    # # model
-    # extractor = torch.jit.load('inception-2015-12-05.pt').eval()
-
-    # labels, preds, paths, features = [], [], [], []
-    # counter = 0
-    # for idx, batch in enumerate(dataloader):
-        
-    #     img_batch = batch['img']
-    #     label_batch = batch['label']
-    #     path_batch = batch['path']
-
-    #     counter += img_batch.size(0)
-
-    #     feature_batch = extractor(img_batch, return_features=True)
-
-    #     # features_repeat = torch.repeat_interleave(feature_batch[:, None, :], repeats=6, dim=1)
-    #     # distmat = (features_repeat - mean) / (std+1e-9) 
-    #     # distmat = torch.norm(distmat, dim=2, p=2) 
-    #     # _, pred_batch = torch.min(distmat, dim=1)
-    #     features.append(feature_batch)
-    #     labels.append(label_batch)
-    #     # preds.append(pred_batch)
-    #     # paths.extend(path_batch)
-    #     print(f"{idx}, {counter}")
-
-    # features = torch.cat(features)
-    # labels = torch.cat(labels)
-    # torch.save(
-    #     {'feat': features, 'label': labels},
-    #     'points.pt'
-    # )
-    # preds = torch.cat(preds)
-
-    # acc = (preds == labels).sum() / counter
-    # print(acc)
-
-    # cm = np.array(confusion_matrix(np.array(labels), np.array(preds)))
-
-    # # labels_name = os.listdir(dir)
-    # plot_confusion_matrix(cm, labelname_list, "CMatrix")
     points = torch.load('points.pt', map_location='cpu')
     features = points['feat'].numpy()
     labels = points['label'].numpy()
 
     plot_TSNE(features, labels, labelname_list, 2)
 
-
-
-# 19个类别，每个类别2048维特征长度
-# features = torch.rand(19, 2048)
 
